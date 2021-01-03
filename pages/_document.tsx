83efc57891f7757584ee3f3df04b1e041c9734db
Pranav Karawale <pdkarawale31@gmail.com>
2021-01-03 22:42:40 +0530

    chore: fix google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,13 +23,15 @@ class MyDocument extends Document {
             async
             src="https://www.googletagmanager.com/gtag/js?id=UA-175190267-1"
           ></script>
-          <script>
-            {`window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'UA-175190267-1');`}
-          </script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+  
+              gtag('config', 'UA-175190267-1');`,
+            }}
+          />
         </Head>
         <body>
           <Main />
